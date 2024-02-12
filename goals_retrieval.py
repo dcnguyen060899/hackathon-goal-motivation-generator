bc7e89e259382cd57469712bec521b742801bbb1
Duy Nguyen <146409192+dcnguyen060899@users.noreply.github.com>
2024-02-12 12:52:57 -0800

    Update goals_retrieval.py

--- goals_retrieval.py
+++ goals_retrieval.py
@@ -54,7 +54,6 @@ from llama_index.embeddings import OpenAIEmbedding
 from llama_index import set_global_service_context
 # """# create database"""
 import json
-from llama_index import Document
 # """# retrieve to-do list vector database"""
 # """# load vector database"""
 class Quotes(BaseModel):
